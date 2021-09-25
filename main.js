2ca7240a257fbb2041a9f913fe0f97469d8f5c82
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-09-25 00:22:32 +0000

    serving static file from public folder

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ import favicon from './favicon.svg'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!!!</h1>
-  <img src="${favicon}" />
+  <img src="/favicon.svg" />
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
